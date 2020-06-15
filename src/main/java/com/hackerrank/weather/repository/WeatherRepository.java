cf20f70dc8702984c53fd8c406222f0cd9056baa
chinmayee02 <49452143+chinmayee02@users.noreply.github.com>
2020-06-15 02:15:00 -0400

    Update WeatherRepository.java
    repo

--- src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
+++ src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
@@ -1,4 +1,8 @@
 package com.hackerrank.weather.repository;
 
-public interface WeatherRepository {
+import com.hackerrank.weather.model.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeatherRepository extends JpaRepository<Weather,long> {
+   void deleteById(long id);
 }

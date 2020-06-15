558062173c3238b44544861a6e45de26f1a6de53
chinmayee02 <49452143+chinmayee02@users.noreply.github.com>
2020-06-15 02:14:12 -0400

    Update WeatherApiRestController.java
    initial

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -1,7 +1,18 @@
 package com.hackerrank.weather.controller;
 
+import com.hackerrank.weather.repository.WeatherRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherApiRestController {
+    private WeatherRepository weatherRepository;
+    @DeleteMapping("/erase")
+    public String deleteWeatherdata(@PathVariable long id) {
+
+        weatherRepository.deleteById(id);
+        return "Deleted ";
+    }
+
 }

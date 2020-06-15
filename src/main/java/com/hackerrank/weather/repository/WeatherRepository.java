b40a1f6a3d2190a07d61386a75983fa62e0aebf9
chinmayee02 <49452143+chinmayee02@users.noreply.github.com>
2020-06-15 02:20:20 -0400

    Update WeatherRepository.java
    repo1

--- src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
+++ src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeatherRepository extends JpaRepository<Weather,long> {
    void deleteById(long id);
+   Weather save(long id);
 }
